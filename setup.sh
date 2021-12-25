03eb80e081b34b881611415d0f75e7d85cf129be
Sudhin Varghese <sudh_chandy@yahoo.com>
2021-12-24 22:03:41 -0600

    Installed node and Visual Studio Code

--- setup.sh
+++ setup.sh
@@ -12,3 +12,15 @@ else
 	mkdir brew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C brew
 	echo "PATH=$HOME/brew/bin:$PATH" > ~/.zshrc
 fi
+
+if exists node; then
+	echo "Node already installed"
+else
+	brew install node
+fi
+
+if exists code; then
+	echo "Visual Code already installed"
+else
+	brew install --cask visual-studio-code
+fi
