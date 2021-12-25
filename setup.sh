822471c392c4f23047c7d1583aba88bc92559905
Sudhin Varghese <sudh_chandy@yahoo.com>
2021-12-24 21:29:14 -0600

    Make script rerunnable by having an exists check for needed software

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-cd $HOME
-mkdir brew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C brew
 
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
 
-echo "PATH=$HOME/brew/bin:$PATH" > ~/.zshrc
+if exists brew; then
+	echo "Brew already installed"
+else
+	cd $HOME
+	mkdir brew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C brew
+	echo "PATH=$HOME/brew/bin:$PATH" > ~/.zshrc
+fi
